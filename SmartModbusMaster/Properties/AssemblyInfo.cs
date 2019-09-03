0ce23f66b26a80b74fa9a1bc5546abfa034b3285
Kerem Bilgiçer <krmbil@windowslive.com>
2019-09-03 10:49:31 +0300

    Version updated Unnecessary libraries removed

--- SmartModbusMaster/Properties/AssemblyInfo.cs
+++ SmartModbusMaster/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SmartModbusMaster")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("It converts Modbus polling function to event. It uses NModbus4")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kr")]
 [assembly: AssemblyProduct("SmartModbusMaster")]
-[assembly: AssemblyCopyright("Copyright KRSoft ©  2015")]
+[assembly: AssemblyCopyright("Copyright KRSoft ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
